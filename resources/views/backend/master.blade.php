f203f18d7b50f131c28c20379f1c26c50551117e
Nusrat Jahan <nusrat.jahan@megasolit.com>
2025-10-11 12:24:17 +0600

    dashboard mastaring

---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   @include('backend.includes.style')
+</head>
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+       
+        <!-- Sidebar Start -->
+        @include('backend.includes.sidebar')
+        <!-- Sidebar End -->
+
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+         @include('backend.includes.navbar')
+            <!-- Navbar End -->
+
+            @yield('content')
+
+            <!-- Footer Start -->
+          @include('backend.includes.footer')
+            <!-- Footer End -->
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+   <!-- JavaScript Libraries -->
+  @include('backend.includes.script')
+</body>
+
+</html>
